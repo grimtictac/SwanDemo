1a85b493155f394e7b631a7b4624c5f0e2c28d5f
Shaun Silson <s.silson@gmail.com>
2018-05-27 17:45:13 +0200

    Create schema and put dummy select statements into data.sql to stop build complaining about it being empty

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,12 @@ create table if not exists tasks
    description varchar(255) not null,
    primary key(id)
 );
+
+create table if not exists users
+(
+   id integer not null,
+   username   varchar(255),
+   first_name varchar(255),
+   last_name  varchar(255),
+   primary key(id)
+);
